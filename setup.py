41a63040250604212bc45157f65732c8fd7850ce
Pruthvi2005 <pruthvireddy618@gmail.com>
2025-08-30 22:33:26 +0530

    Update build configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lung_cancer_prediction",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        'Flask==2.0.1',
+        'Flask-SQLAlchemy==2.5.1',
+        'Flask-Login==0.5.0',
+        'Flask-Cors==3.0.10',
+        'Werkzeug==2.0.3',
+        'Jinja2==3.0.3',
+        'itsdangerous==2.0.1',
+        'click==8.0.4',
+        'pandas==1.3.3',
+        'numpy==1.21.6',
+        'scipy==1.7.3',
+        'scikit-learn==0.24.2',
+        'joblib==1.1.0',
+        'python-dotenv==0.19.2',
+        'aws-wsgi==0.2.3',
+    ],
+    python_requires='>=3.7',
+)
